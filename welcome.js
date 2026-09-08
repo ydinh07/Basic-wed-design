2c844403bb29dccd5a905aa38149aa93f0323c1c
Ho Thi Y Dinh <ydinh7803gmail.com>
2026-09-08 22:00:13 +0700

    bai tap thuc hanh

---
+++ welcome.js
@@ -0,0 +1,8 @@
+
+        // alert(" Welcome to my wedsite") 
+        console.log("Welcome to my wedsite ")
+let sum = 0;
+let a = 10;
+let b = 20;
+ sum = a + b;
+console.log(" The sun of a and b is: " + sum)
